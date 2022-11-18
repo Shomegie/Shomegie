75f1682c249b204c2a8c9d19b92836bf3ebf93f7
Shomegie <shomegie@gmail.com>
2022-11-18 20:06:31 +0100

    mobile fullscreen fix attempt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     let menu =`text-black text-opacity-60 font-thin cursor-pointer hover:text-opacity-100 transition duration-150 py-2 px-3`
 </script>
 
-<div class="flex flex-col h-[100vh] bg-[#F7DEC8] w-full relative sm:hidden">
+<!-- <div class="flex flex-col h-[100vh] bg-[#F7DEC8] w-full relative sm:hidden">
     <div class="absolute trap w-1/2 "></div>
     <div class="z-10 h-1/4 w-full px-8 pt-8">
         <div class="menu flex justify-end">
@@ -26,10 +26,34 @@
             <div class="w-fit bg-black text-white mx-auto rounded-full py-3 px-5 text-sm cursor-pointer hover:bg-blue-700 hover:text-blac transition duration-200">Download Resume</div>
         </div>
     </div>
+</div> -->
 
-
-
-
+<div class="absolute sm:hidden inset-0 overflow-hidden">
+    <div class="helv flex flex-col h-full bg-[#F7DEC8] w-full relative">
+        <div class="absolute trap w-1/2 "></div>
+        <div class="z-10 h-1/4 w-full px-8 pt-8">
+            <div class="menu flex justify-end">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 cursor-pointer h-8 opacity-70">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                  </svg>
+                  
+            </div>
+            <div class="name flex flex-col w-2/3 ">
+                <div class="border-b border-black border-opacity-5 tracking-tight font-bold text-black text-opacity-70">Shomegie Ikerodah</div>
+                <div class="tracking-tight text-xs text-black text-opacity-70"> Fullstack developer, frontend designer, UI/UX designer, Svelte/Sveltekit developer, Tailwind developer</div>
+            </div>
+        </div>
+        <div class="z-10 h-2/4 flex flex-col items-center justify-center">
+            <div class="flex flex-col w-fit text-3xl tracking-tight text-black text-opacity-60 pt-16">
+                <div class="">Hello, my name is</div>
+                <div class=""><span class="tracking-tighter text-black text-opacity-75 font-bold">Shomegie Ikerodah </span>& I'm a</div>
+                <div class="">fullstack web developer.</div>
+            </div>
+            <div class="z-10 px-8 pt-10 ">
+                <div class="w-fit bg-black text-white mx-auto rounded-full py-3 px-5 text-sm cursor-pointer hover:bg-blue-700 hover:text-blac transition duration-200">Download Resume</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
