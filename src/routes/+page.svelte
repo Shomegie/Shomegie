8f62ef68712b84c0177bff77b3fc53a507dd9086
Shomegie <shomegie@gmail.com>
2022-11-18 01:15:00 +0100

    mobile & desktop views for landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1 class="text-red-300">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let menu =`text-black text-opacity-60 font-thin cursor-pointer hover:text-opacity-100 transition duration-150 py-2 px-3`
+</script>
+
+<div class="flex flex-col h-[100vh] bg-[#F7DEC8] w-full relative sm:hidden">
+    <div class="absolute trap w-1/2 "></div>
+    <div class="z-10 h-1/4 w-full px-8 pt-8">
+        <div class="menu flex justify-end">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 cursor-pointer h-8 opacity-70">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              
+        </div>
+        <div class="name flex flex-col w-2/3 ">
+            <div class="border-b border-black border-opacity-5 tracking-tight font-bold text-black text-opacity-70">Shomegie Ikerodah</div>
+            <div class="tracking-tight text-xs text-black text-opacity-70"> Fullstack developer, frontend designer, UI/UX designer, Svelte/Sveltekit developer, Tailwind developer</div>
+        </div>
+    </div>
+    <div class="z-10 h-2/4 flex flex-col items-center justify-center">
+        <div class="flex flex-col w-fit text-3xl tracking-tight text-black text-opacity-60 pt-16">
+            <div class="">Hello, my name is</div>
+            <div class=""><span class="tracking-tighter text-black text-opacity-75 font-bold">Shomegie Ikerodah </span>& I'm a</div>
+            <div class="">fullstack web developer.</div>
+        </div>
+        <div class="z-10 px-8 pt-10 ">
+            <div class="w-fit bg-black text-white mx-auto rounded-full py-3 px-5 text-sm cursor-pointer hover:bg-blue-700 hover:text-blac transition duration-200">Download Resume</div>
+        </div>
+    </div>
+
+
+
+
+</div>
+
+
+
+<div hidden class="h-[100vh] bg-[#F7DEC8] w-full sm:flex flex-col helv">
+    <div class="h-[240px] bg-[#F2FDFF] w-full flex items-center justify-center">
+        <div class=" w-fit flex flex-col">
+            <div class="helv text-6xl lg:text-7xl tracking-tight text-black text-opacity-80 ">Shomegie Ikerodah</div>
+            <div class="helv text-sm pl-1 text-black text-opacity-80 lg:tracking-wider lg:text-base">Fullstack developer, frontend designer, UI/UX designer, Svelte/Sveltekit developer, Tailwind developer</div>
+        </div>
+    </div>
+    <div class="menu pt-3">
+        <ul class="flex w-4/5 md:w-4/5 lg:w-4/6 xl:w-4/6 xl:px-10 mx-auto justify-between">
+            <li class={menu}>Home</li>
+            <li class={menu}>About</li>
+            <li class={menu}>Contact</li>
+            <li class={menu}>Projects</li>
+        </ul>
+    </div>
+    
+    <div class="content h-fit w-full">
+        <div class="flex flex-col w-fit mx-auto text-5xl xl:text-6xl text-black text-opacity-60 pt-10 space-y-3">
+            <div class="border-b border-black w-fit pr-6 border-opacity-10">Hello, my name is</div>
+            <div class="border-b border-black w-fit pr-6 border-opacity-10"><span class="text-black text-opacity-80 font-semibold tracking-tight ">Shomegie Ikerodah </span>& I'm a</div>
+            <div class="border-b border-black w-fit pr-1 border-opacity-10">fullstack web developer.</div>
+        </div>
+    </div>
+
+    <div class="pt-10">
+        <div class="w-fit bg-black text-white mx-auto rounded-full py-3 px-5 text-sm cursor-pointer hover:bg-blue-700 hover:text-white transition duration-200">Download Resume</div>
+    </div>
+
+</div>
+
+<style>
+    .helv{
+        font-family: Helvetica, sans-serif;
+    }
+    .trap{
+        border-bottom: 100vh solid #F2FDFF;
+        border-right: 35vw solid transparent;
+    }
+</style>
---
+++ src/routes/sandbox/+page.svelte
@@ -0,0 +1,11 @@
+<div class="w-full h-[80vh] bg-teal-100 ">
+    <div class="trapp h-full w-1/2"></div>
+
+</div>
+
+<style>
+    .trapp{
+        border-bottom: 100vh solid red;
+        border-right: 35vw solid transparent;
+    }
+</style>
