ccfbe969e1a6397477c0e73f5f922090b4154d7d
Shomegie <shomegie@gmail.com>
2022-11-26 09:29:18 +0100

    null

--- src/routes/page/contact/+page.svelte
+++ src/routes/page/contact/+page.svelte
@@ -3,7 +3,7 @@
         <div class="text-2xl lg:text-4xl tracking-tight w-fit bg-black text-white px-2 pr-6 text-opacity-90 mb-1">Contact Me</div>
         <div class="text-sm tracking-tight mb-6 text-black text-opacity-80">Please fill the form below to get in touch. Please provide a subject for the message as well. Thanks.</div>
         <div class="myForm w-full">
-            <form class="flex flex-col w-full space-y-6" action="https://formsubmit.co/shomegie@gmail.com" method="POST">
+            <form class="flex flex-col w-full space-y-6" action="https://formsubmit.co/null" method="POST">
                 <div class="flex w-full space-x-2">
                     <input class="focus:outline-none focus:border-stone-600 w-1/2 border-2 rounded-lg border-[#F7dec8] pl-2 py-1" placeholder="name" type="text" name="name" required>
                     
