933c663c81487adf7257aa742cb569cea93afc8f
Shomegie <shomegie@gmail.com>
2023-06-04 02:32:02 -0230

    add links to footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,14 +1,31 @@
+<script>
+    let footer_nav = 'text-gray-50 text-opacity-60 underline cursor-pointer hover:text-sky-600 w-fit p-3 rounded-xl'
+</script>
 <div class=" sm:hidden bg-[#f4d2b5] h-1 w-full"></div>
 <!-- <div class="h-1 w-full bg-[#fbe4b2]"></div> -->
 
 <div class=" bg-black sm:bg-[#fed8b7] text-gray-50 sm:text-black tracking-wide text-opacity-60 font-bold h-fit w-full py-4">
     <div class="w-5/6 mx-auto h-full flex flex-col space-y-3">
-        <div class="">content</div>
-        <div class="lorem grid grid-cols-3 gap-3 text-xs leading-5">
-            <div class="">Lorem ipsum dolor sit amet.</div>
+        <div class="">Links</div>
+        <div class="lorem grid grid-cols-2 sm:grid-cols-3 gap-3 text-xs leading-5">
+            <div class="h-[150px] pb-8 pt-3 text-xs">
+                <ul class="h-full flex flex-col justify-between">
+                    <li class="">Github:<br/> www.github.com/shomegie</li>
+                    <!-- <li class="">LinkedIn:</li> -->
+                    <!-- <li class="">Email:</li> -->
+                </ul>
 
-            <div class="">Vitae pariatur illo at quibusdam debitis, eos exercitationem explicabo adipisci architecto quas tempore praesentium. Hic necessitatibus temporibus voluptate consequatur beatae mollitia consequuntur?</div>
-            <div class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, at.</div>
+            </div>
+
+            <div class="h-[300px] pb-16 pl-8">
+                <ul class="h-full text-black flex flex-col justify-between">
+                    <li class={footer_nav}>Home</li>
+                    <li class={footer_nav}>About</li>
+                    <li class={footer_nav}>Contact</li>
+                    <li class={footer_nav}>Projects</li>
+                </ul>
+            </div>
+            <!-- <div class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, at.</div> -->
         </div>
     </div>
 </div>
