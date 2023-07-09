3dc1f0885ed9e1826c7eda0958972d4204696f9b
Shomegie <shomegie@gmail.com>
2023-07-08 22:39:33 -0230

    amber-100 for project tiles

--- src/components/project_card_2.svelte
+++ src/components/project_card_2.svelte
@@ -1,10 +1,20 @@
-<div class="w-full h-44 bg-lime-100 flex">
+<!-- <div class="w-full h-44 bg-lime-100 flex">
     <div class="h-full w-1/3 bg-stone-200"></div>
     <div class="h-full w-2/3 py-2 pl-2 pr-2 -space-y-1 relative">
         <div class="text-xl tracking-tight font-bold m-0 p-0">Spotify Artist Top Track Preview</div>
-        <div class="text-sm text-stone-800 m-0 p-0">[Svelte, Sveltekit, Tailwind, HTML, CSS, JavaScript]</div>
+        <div class="text-sm text-blue-700 m-0 p-0">[Svelte, Sveltekit, Tailwind, HTML, CSS, JavaScript]</div>
         <p class="pt-4">Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos harum ipsa deserunt.</p>
         <div class="links absolute bottom-1 text-sky-600">[github] [live-site]</div>
     </div>
+</div> -->
 
+<div class="w-full h-44 bg-amber-100 flex">
+    <div class="h-full w-1/3 bg-stone-400"></div>
+    <div class="h-full w-2/3 py-2 pl-2 pr-2 -space-y-1 relative">
+        <div class="text-xl tracking-tight font-bold m-0 p-0">Spotify Artist Top Track Preview</div>
+        <div class="text-sm text-blue-700 m-0 p-0">[Svelte, Sveltekit, Tailwind, HTML, CSS, JavaScript]</div>
+        <p class="pt-4">Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos harum ipsa deserunt.</p>
+        <div class="links absolute bottom-1 text-sky-600">[github] [live-site]</div>
+    </div>
 </div>
+
