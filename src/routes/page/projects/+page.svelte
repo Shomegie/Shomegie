dcce6931c35f5601592dcd5d272774fcdf990b9c
Shomegie <shomegie@gmail.com>
2023-07-12 13:41:13 -0230

    revise color scheme

--- src/routes/page/projects/+page.svelte
+++ src/routes/page/projects/+page.svelte
@@ -28,31 +28,36 @@
 
 
 </script>
+<div class="w-full pt-6">
+    <div class="min-h-[50vh] flex flex-col w-5/6 sm:w-4/6 mx-auto">
+        <div class="text-2xl lg:text-3xl tracking-tight w-fit bg-[#f7dec8] text-black py-1 px-2 pr-10 text-opacity-90 mb-1">Projects</div>
+        <div class="w-full border-b border-stone-50 mx-auto mt-4 mb-3 divider"></div>
 
-<div class=" pt-16 min-h-[50vh] w-full">
-    <!-- <div class="w-5/6 mx-auto pb-4">
-        <div class="text-3xl lg:text-4xl tracking-tight w-fit bg-[#f7dec8] text-black px-2 py-1 pr-10 text-opacity-90">Projects</div>
-    </div> -->
+
+        <!-- <div class="w-5/6 mx-auto pb-4">
+            <div class="text-3xl lg:text-4xl tracking-tight w-fit bg-[#f7dec8] text-black px-2 py-1 pr-10 text-opacity-90">Projects</div>
+        </div> -->
 
 
-    <div class="w-3/4 mx-auto grid grid-cols-2 gap-8 ">
-        {#each projects as project }
-            <ProjectCard_2 {project} />
-        {/each}
+        <div class="w-full mt-2 mx-auto grid grid-cols-2 gap-8 ">
+            {#each projects as project }
+                <ProjectCard_2 {project} />
+            {/each}
+        </div>
+
+    <!-- 
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-6 md:gap-10  w-fit mx-auto lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+
+        </div> -->
+
+
+
     </div>
-
-<!-- 
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-6 md:gap-10  w-fit mx-auto lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
-
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-
-    </div> -->
-
-
-
 </div>
