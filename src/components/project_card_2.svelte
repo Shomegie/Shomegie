a89b165bb21022e49d8372df9a6b486a795faa0a
Shomegie <shomegie@gmail.com>
2023-07-22 13:21:33 -0230

    separate streams for link buttons on project page

--- src/components/project_card_2.svelte
+++ src/components/project_card_2.svelte
@@ -9,6 +9,8 @@
 </div> -->
 <script>
     export let project;
+
+    let link_buttons = 'text-sky-800 px-1 text-sm bg-red-600 h-fit'
 </script>
 
 <!-- <div class="w-full h-40 lg:h-40 bg-gray-800 flex text-sky-400 cursor-pointer hover:bg-gray-900 transition duration-300">
@@ -22,8 +24,8 @@
 </div> -->
 <div class="flex flex-col h-fit">
     <div class="w-full h-24 md:h-32 lg:h-40 bg-gray-800 flex text-sky-400 cursor-pointer hover:bg-gray-900 transition duration-300">
-        <div class="h-full hidden sm:flex w-1/3 bg-[#555] border border-orange-200 border-r-0"></div>
-        <div class="h-full w-full sm:w-2/3 py-2 pl-2 pr-2 -space-y-1 relative border border-orange-200 border-l-0">
+        <div class="h-full hidden sm:flex w-1/3 bg-[#555] border border-b-0 border-orange-200 border-r-0"></div>
+        <div class="h-full w-full sm:w-2/3 py-2 pl-2 pr-2 -space-y-1 relative border border-orange-200 border-b-0 border-l-0">
             <div class="text-md lg:text-xl tracking-tight font-bold leading-4 m-0 p-0">{project.name}</div>
             <div class="text-xs lg:text-md text-blue-500 m-0 pt-1 lg:pt-0">[{project.tech}]</div>
             <p class="pt-2 lg:pt-4 text-gray-300 text-xs lg:text-md">{project.desc}</p>
@@ -31,9 +33,9 @@
         </div>
     </div>
     <div class="bg-orange-300 lg:bg-orange-200 h-8 w-full relative">
-        <div class="flex lg:hidden w-full justify-end">
-            <div class=""></div>
-            <div class="">Lester</div>
+        <div class="flex lg:hidden w-full justify-end space-x-2 h-full mx-auto pt-1">
+            <div class={link_buttons}>[github]</div>
+            <div class={link_buttons}>[live-site]</div>
         </div>
         <div class="links hidden lg:flex absolute bottom-1 text-sky-700 font-bold space-x-2 justify-end w-full pr-8"><span class="hover:text-gray-800 cursor-pointer">[github]</span><span class="cursor-pointer hover:text-gray-900">[live-site]</span></div>
 
