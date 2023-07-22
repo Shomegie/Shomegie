5fb8138c694341d583f4dce60f3254ec5f3254ab
Shomegie <shomegie@gmail.com>
2023-07-22 13:30:14 -0230

    complete responsive design of project page

--- src/components/project_card_2.svelte
+++ src/components/project_card_2.svelte
@@ -9,19 +9,8 @@
 </div> -->
 <script>
     export let project;
-
-    let link_buttons = 'text-sky-800 px-1 text-sm bg-red-600 h-fit'
 </script>
 
-<!-- <div class="w-full h-40 lg:h-40 bg-gray-800 flex text-sky-400 cursor-pointer hover:bg-gray-900 transition duration-300">
-    <div class="h-full w-1/3 bg-[#555] border border-orange-200 border-r-0"></div>
-    <div class="h-full w-2/3 py-2 pl-2 pr-2 -space-y-1 relative border border-orange-200 border-l-0">
-        <div class="text-xl tracking-tight font-bold m-0 p-0">{project.name}</div>
-        <div class="text-sm text-blue-500 m-0 p-0">[{project.tech}]</div>
-        <p class="pt-4 text-gray-300">{project.desc}</p>
-        <div class="links absolute bottom-1 text-sky-600 flex space-x-2 justify-end w-full pr-8 pb-2"><span class="hover:text-orange-200">[github]</span><span class="hover:text-orange-200">[live-site]</span></div>
-    </div>
-</div> -->
 <div class="flex flex-col h-fit">
     <div class="w-full h-24 md:h-32 lg:h-40 bg-gray-800 flex text-sky-400 cursor-pointer hover:bg-gray-900 transition duration-300">
         <div class="h-full hidden sm:flex w-1/3 bg-[#555] border border-b-0 border-orange-200 border-r-0"></div>
@@ -33,11 +22,6 @@
         </div>
     </div>
     <div class="bg-orange-300 lg:bg-orange-200 h-8 w-full relative">
-        <div class="flex lg:hidden w-full justify-end space-x-2 h-full mx-auto pt-1">
-            <div class={link_buttons}>[github]</div>
-            <div class={link_buttons}>[live-site]</div>
-        </div>
-        <div class="links hidden lg:flex absolute bottom-1 text-sky-700 font-bold space-x-2 justify-end w-full pr-8"><span class="hover:text-gray-800 cursor-pointer">[github]</span><span class="cursor-pointer hover:text-gray-900">[live-site]</span></div>
-
+        <div class="links flex absolute text-sm lg:text-base bottom-1 text-sky-700 font-bold space-x-2 justify-end w-full pr-2 lg:pr-8"><span class="hover:text-gray-800 cursor-pointer">[github]</span><span class="cursor-pointer hover:text-gray-900">[live-site]</span></div>
     </div>
 </div>
--- src/routes/page/projects/+page.svelte
+++ src/routes/page/projects/+page.svelte
@@ -33,31 +33,10 @@
         <div class="text-2xl lg:text-3xl tracking-tight w-fit bg-[#facfa8] text-black py-1 px-2 pr-10 text-opacity-90 mb-1">Projects</div>
         <div class="w-full border-b border-stone-50 mx-auto mt-1 mb-3 divider"></div>
 
-
-        <!-- <div class="w-5/6 mx-auto pb-4">
-            <div class="text-3xl lg:text-4xl tracking-tight w-fit bg-[#f7dec8] text-black px-2 py-1 pr-10 text-opacity-90">Projects</div>
-        </div> -->
-
-
         <div class="w-full mt-1 mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-8 ">
             {#each projects as project }
                 <ProjectCard_2 {project} />
             {/each}
         </div>
-
-    <!-- 
-        <div class="grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-6 md:gap-10  w-fit mx-auto lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
-
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-
-        </div> -->
-
-
-
     </div>
 </div>
